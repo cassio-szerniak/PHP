4bc2e119a2369a26f6a0eb291818b50d261b762c
Cassio Szerniak <cassioszerniak@gmail.com>
2023-05-22 21:15:56 +0100

    Update 03-primo.php

--- Aulas/php-aula13/aula13/03-primo.php
+++ Aulas/php-aula13/aula13/03-primo.php
@@ -1,22 +1,29 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-  <link rel="stylesheet" href="_css/estilo.css"/>
-  <meta charset="UTF-8"/>
-  <title>Curso de PHP - CursoemVideo.com</title>
-  <style>
-	h1 { font: 20pt Arial; }
-	h2 { font: 18pt Arial; }
-  </style>
+	<link rel="stylesheet" href="_css/estilo.css" />
+	<meta charset="UTF-8" />
+	<title>Curso de PHP - CursoemVideo.com</title>
+	<style>
+		h1 {
+			font: 20pt Arial;
+		}
+
+		h2 {
+			font: 18pt Arial;
+		}
+	</style>
 </head>
+
 <body>
-<div>
-    <?php
-       $n = isset($_GET["num"])?$_GET["num"]:1;
-	   echo "<h1>Analisando o n&uacute;mero $n ...</h1>";
-	   echo "Valores m&uacute;ltiplos: ";
-	   $mult = 0;
-	   for ($c = 1; $c <= $n; $c++) {
+	<div>
+		<?php
+		$n = isset($_GET["num"]) ? $_GET["num"] : 1;
+		echo "<h1>Analisando o n&uacute;mero $n ...</h1>";
+		echo "Valores m&uacute;ltiplos: ";
+		$mult = 0;
+		for ($c = 1; $c <= $n; $c++) {
 			if ($n % $c == 0) {
 				$mult++;
 				echo "$c ";
@@ -29,11 +36,11 @@
 		} else {
 			echo " N&Atilde;O &Eacute; PRIMO! ";
 		}
-		 echo "</span></h2>";
-    ?>
-	<br/>
-	<a href="javascript:history.go(-1)" class="botao">Voltar</a>
-</div>
+		echo "</span></h2>";
+		?>
+		<br />
+		<a href="javascript:history.go(-1)" class="botao">Voltar</a>
+	</div>
 </body>
+
 </html>
- 
